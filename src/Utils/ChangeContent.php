6d020745e6ab13f244c8590211ede1095a7da90f
MaxiM <mmirrev@mail.ru>
2018-11-22 21:18:23 +0300

    remade in OOP style

--- src/Utils/ChangeContent.php
+++ src/Utils/ChangeContent.php
@@ -1,36 +1,56 @@
 <?php
 
+    namespace FM\Utils;
+
 	require_once "../config/Conf.php";
 
-	$data = [
-		'msg' => '',
-		'result' => 'error'
-    ];
-	        
-	$code = $_POST['code'];
-	$path = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_PATH);
-	$relativePath = preg_replace('/\/'. FM_FOLDER_NAME .'/', '', $path, 1);
-	$query = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_QUERY);
-	$urlQuery = ($query != '') ? str_replace('url=', '', $query) : '';
-	$pathFile = ROOT . $relativePath . $urlQuery;
+	class ChangeContent
+    {
+        private $code;
+        private $pathFile;
+        private $data = [
+            'msg' => '',
+            'result' => 'error'
+        ];
 
-	if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-		$data['msg'] = "Incorrect method of sending data <br>";
-	} else {
-        if (!is_writable($pathFile)) {
-            $data['msg'] .=  'File is not writable <br>';
-        } else {    
-        	$handle = fopen($pathFile, 'w'); 
-        	if ($handle) {
-        		try {
-        			fwrite($handle, $code);
-        		} finally {
-        			fclose($handle);
-        			$data['result'] =  'success';
-        		}
-        	}
-        }	    
-	}	
+        public function __construct()
+        {
+            if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+                $this->data['msg'] = "Incorrect method of sending data <br>";
+            } else {
+                $this->code = $_POST['code'];
+                $path = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_PATH);
+                $relativePath = preg_replace('/\/'. FM_FOLDER_NAME .'/', '', $path, 1);
+                $query = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_QUERY);
+                $urlQuery = ($query != '') ? str_replace('url=', '', $query) : '';
+                $this->pathFile = ROOT . $relativePath . $urlQuery;
+                $this->writeFile();
+            }
+        }
 
-	header('Content-Type: application/json');
-	echo json_encode($data);	
+        private function writeFile()
+        {
+            if (!is_writable($this->pathFile)) {
+                $this->data['msg'] .=  'File is not writable <br>';
+            } else {
+                $handle = fopen($this->pathFile, 'w');
+                if ($handle) {
+                    try {
+                        fwrite($handle, $this->code);
+                    } finally {
+                        fclose($handle);
+                        $this->data['result'] =  'success';
+                    }
+                }
+            }
+        }
+
+        public function echoJsonEncode()
+        {
+            header('Content-Type: application/json');
+            echo json_encode($this->data);
+        }
+    }
+
+    $newContent = new ChangeContent();
+    $newContent->echoJsonEncode();
