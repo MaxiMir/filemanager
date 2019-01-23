cf83bb718934883e510a32f990568a9358a8af42
MaxiM <mmirrev@mail.ru>
2019-01-23 22:13:17 +0300

    add function for search phrase highlighting

--- js/codeEditor.js
+++ js/codeEditor.js
@@ -1,93 +1,100 @@
 $(function () {
-	$('[data-toggle="tooltip"]').tooltip();
+    const encodeSearchText = getEncodeSearchText();
 
-	const editor = ace.edit('editor');
+    $('[data-toggle="tooltip"]').tooltip();
+
+    editor = ace.edit('editor');
     const mode = setEditorMode();
     const relUrl = $('main').attr('data-relurl');
     let source = editor.getValue();
-    
-	editor.session.setMode('ace/mode/' + mode);
-	editor.setOptions({
-		minLines: 15,
-		autoScrollEditorIntoView: true,
-		copyWithEmptySelection: true,
-		wrap: true
-	});
 
-	$('#editor').on('change input paste cut keyup keydown', function (e) {
-		if (source === editor.getValue()) {
-			$('#btnSave, #btnRemove').attr('disabled', 'disabled');
-		} else {
-			if (e.which == 83 && e.ctrlKey) {
-				e.preventDefault();
-				$('#btnSave').trigger('click');
-			}
-			$('#btnSave, #btnRemove').prop('disabled', false);
-		}
-	});
+    editor.session.setMode('ace/mode/' + mode);
+    editor.setOptions({
+        minLines: 15,
+        autoScrollEditorIntoView: true,
+        copyWithEmptySelection: true,
+        wrap: true
+    });
 
-	$('#btnRemove').on('click', function () {
-		editor.setValue(source);
-	});
 
-	$('#btnSave').on('click', function () {
+    $('#editor').on('change input paste cut keyup keydown', function (e) {
+        if (source === editor.getValue()) {
+            $('#btnSave, #btnRemove').attr('disabled', 'disabled');
+        } else {
+            if (e.which == 83 && e.ctrlKey) {
+                e.preventDefault();
+                $('#btnSave').trigger('click');
+            }
+            $('#btnSave, #btnRemove').prop('disabled', false);
+        }
+    });
+
+    $('#btnRemove').on('click', function () {
+        editor.setValue(source);
+    });
+
+    $('#btnSave').on('click', function () {
         const code = editor.getValue();
-        const msg = $('.main-message');
 
-		$.post({
-			url: relUrl + 'Utils/ChangeContent.php',
-			data: {
-				'code': code
-			},
-			beforeSend: function () {
-				$('#btnSave, #btnRemove').attr('disabled', 'disabled');
-			},
-			success: function (data) {
+        $.post({
+            url: relUrl + 'Utils/ChangeContent.php',
+            data: {
+                'code': code
+            },
+            beforeSend: function () {
+                $('#btnSave, #btnRemove').attr('disabled', 'disabled');
+            },
+            success: function (data) {
                 const text = data['result'] === 'success' ? 'File has been successfully modified' : data['msg'];
-				if (data['result'] === 'success') {
-					source = code;
-				}
+                if (data['result'] === 'success') {
+                    source = code;
+                }
 
                 showThenHideMsg(text);
-			},
-			error: errorHandler = function () {
+            },
+            error: errorHandler = function () {
                 showThenHideMsg('Error loading files', true);
-			}
-		});
-	});
+            }
+        });
+    });
 
-	$('.lamp').on('click', function () {
-		if ($(this).is(':checked')) {
-			editor.setTheme('ace/theme/ambiance');
-		} else {
-			editor.setTheme('ace/theme/eclipse');
-		}
-	});
+    $('.lamp').on('click', function () {
+        if ($(this).is(':checked')) {
+            editor.setTheme('ace/theme/ambiance');
+        } else {
+            editor.setTheme('ace/theme/eclipse');
+        }
+    });
+
+    if (encodeSearchText) {
+        showSearchPhrase(encodeSearchText);
+    }
+
 });
 
 function setEditorMode() {
 
-	const ext = $('#editor').attr('data-ext');
+    const ext = $('#editor').attr('data-ext');
     const modes = {
-			'php': 'php',
-			'js': 'javascript',
-			'py': 'python',
-			'rb': 'ruby',
-			'json': 'json',
-			'sql': 'sql',
-			'html': 'html',
-			'twig': 'twig',
-			'xml': 'xml',
-			'yaml': 'yaml',
-			'css': 'css',
-			'sass': 'sass',
-			'scss': 'scss',
-			'less': 'less',
-			'svg': 'svg',
-			'slim': 'slim'
-		};
+        'php': 'php',
+        'js': 'javascript',
+        'py': 'python',
+        'rb': 'ruby',
+        'json': 'json',
+        'sql': 'sql',
+        'html': 'html',
+        'twig': 'twig',
+        'xml': 'xml',
+        'yaml': 'yaml',
+        'css': 'css',
+        'sass': 'sass',
+        'scss': 'scss',
+        'less': 'less',
+        'svg': 'svg',
+        'slim': 'slim'
+    };
 
-	return ext in modes ? modes[ext] : 'text';
+    return ext in modes ? modes[ext] : 'text';
 }
 
 function showThenHideMsg(msg, error = null) {
@@ -101,4 +108,18 @@ function showThenHideMsg(msg, error = null) {
         .show()
         .delay(showTime)
         .fadeOut(100);
+}
+
+function getEncodeSearchText() {
+    const uri = $(location).attr('href');
+    const uriData = uri.split('#find=');
+
+    return uriData.length === 2 ? uriData[1] : false;
+}
+
+function showSearchPhrase(encodeSearchText) {
+    const decodeSearchText = decodeURIComponent(escape(window.atob(encodeSearchText)));
+
+    editor.find(decodeSearchText);
+
 }
