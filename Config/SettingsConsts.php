2075e499180a5540a5d6419e15220ab39f74d830
MaxiM <mmirrev@mail.ru>
2019-02-07 23:53:58 +0300

    correction namespace

--- Config/SettingsConsts.php
+++ Config/SettingsConsts.php
@@ -1,15 +1,13 @@
 <?php
 
-    namespace
-    {
-        define('SEP', DIRECTORY_SEPARATOR);
-	    define('ROOT', $_SERVER['DOCUMENT_ROOT'] . SEP);
-	    define('FM_FOLDER_NAME', 'fm');
-	    define('FM_PATH', dirname(__DIR__) . SEP);
-	    define('FM_REL_PATH', str_replace(ROOT, '/', FM_PATH));
-	    define('TIME_NEW_FILE', 60);
-    }
+   namespace FM\Config\SettingsConsts;
 
+   define('SEP', DIRECTORY_SEPARATOR);
+   define('ROOT', $_SERVER['DOCUMENT_ROOT'] . SEP);
+   define('FM_FOLDER_NAME', 'fm');
+   define('FM_PATH', dirname(__DIR__) . SEP);
+   define('FM_REL_PATH', str_replace(ROOT, '/', FM_PATH));
+   define('TIME_NEW_FILE', 60);
 
 
 
